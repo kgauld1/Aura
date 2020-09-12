5e08a27f7ff2b182676479cd40e43925f6a5a6b7
cew88 <cew8@rice.edu>
2020-09-12 00:07:17 +0000

    Fixed animation background

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ app.use(express.static(__dirname + "/public"));
 app.get('/', (req, res) => {
   res.sendFile('index');
 });
-app.get('/home', (req, res) => {
-  res.sendFile(__dirname + '/public/levels.html');
-});
 
 app.listen(3000, () => {
   console.log('server started');
 });
-
-
